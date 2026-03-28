94c89d504745b2ff07c54b68af78ac5f2ea37213
Luis Eric Lima <125391123+luisericlima@users.noreply.github.com>
2026-03-28 19:54:57 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,8 @@ module.exports = defineConfig({
     specPattern: "cypress/e2e/**/*.cy.js",
     supportFile: "cypress/support/e2e.js",
     retries: 2,
+    defaultCommandTimeout: 10000, 
+    pageLoadTimeout: 60000,
     viewportWidth: 1366,
     viewportHeight: 768,
     video: true, 
